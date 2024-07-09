011d7be0c6f689ab83ed8b2c2e6f96f7b70f1f49
Marasingha-MMKGB <kalendramarasingha@gmail.com>
2024-07-09 22:34:22 +0530

    WUR enabled IOT Network
    a network that is power efficient due to implemention of WUR
    
    - Implemented the WUS(wakeup Signal And Main radio Application of Sending INfo)
    - Turn on and Off Via WuC(Wakeup Call)

---
+++ Implementing WuR on Sensor network/out/clang-debug/server.o.d
@@ -0,0 +1,4 @@
+out/clang-debug//server.o: server.cc \
+  C:/Network/omnetpp-6.0.3-windows-x86_64/omnetpp-6.0.3/include/omnetpp.h
+
+C:/Network/omnetpp-6.0.3-windows-x86_64/omnetpp-6.0.3/include/omnetpp.h:
